14a49f55be3726eec6ad3fd4af5bd3327a13ac13
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2018-10-31 00:06:06 +0100

    Implement quotes api

--- fachschaftszitat/fachschaftszitat/api/urls.py
+++ fachschaftszitat/fachschaftszitat/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from fachschaftszitat.api import views
 app_name = 'fachschaftszitat.api'
 urlpatterns = [
-    path('quote/latest/', views.ApiGetLatestQuote.as_view(), name='test'),
+    path('quote/latest/', views.ApiGetLatestQuote.as_view(), name='last-quote'),
+    path('quote/', views.ApiGetQuotes.as_view(), name='quotes'),
 ]
--- fachschaftszitat/fachschaftszitat/api/views.py
+++ fachschaftszitat/fachschaftszitat/api/views.py
@@ -1,5 +1,5 @@
 from django.templatetags.static import static
-from rest_framework import views, status
+from rest_framework import views, status, generics
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -18,3 +18,7 @@ class ApiGetLatestQuote(views.APIView):
         results = QuoteSerializer(Quote.objects.last(), many=False).data
         return Response(results, status=status.HTTP_200_OK)
 
+
+class ApiGetQuotes(generics.ListAPIView):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
