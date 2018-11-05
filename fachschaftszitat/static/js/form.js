f9fe5ca55557a66e7e8b10e99357f4c0dc037d2b
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2018-11-06 00:11:47 +0100

    Fix wrong csrf token on ajax

--- fachschaftszitat/static/js/form.js
+++ fachschaftszitat/static/js/form.js
@@ -1,6 +1,42 @@
+/**
+ * setup JQuery's AJAX methods to setup CSRF token in the request before sending it off.
+ * http://stackoverflow.com/questions/5100539/django-csrf-check-failing-with-an-ajax-post-request
+ */
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 $("#quote-form").submit(function (event) {
     event.preventDefault();
-    var form = $('#author-form');
+    var form = $('#quote-form');
     var url = form.attr('action');
     $.ajax({
         url: url,
