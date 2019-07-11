ddf1783c90bf15086a1ce66d3b49b7442f3f53a3
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2019-07-11 19:13:50 +0200

    Add owner column, Rewrite main html code

--- fachschaftszitat/fachschaftszitat/api/serializer.py
+++ fachschaftszitat/fachschaftszitat/api/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from fachschaftszitat.models import Quote, Statement, Author
+from django.contrib.auth.models import Group
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -16,9 +17,16 @@ class StatementSerializer(serializers.ModelSerializer):
         fields = ('id', 'order_id', 'text', 'author')
 
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'name')
+
+
 class QuoteSerializer(serializers.ModelSerializer):
     statements = StatementSerializer(many=True)
+    owner = GroupSerializer()
 
     class Meta:
         model = Quote
-        fields = ('id', 'timestamp', 'statements')
+        fields = ('id', 'timestamp', 'statements', 'owner')
