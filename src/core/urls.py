c8a29d4446a65cedaac12ca95c8e6cb121a8583b
MG <mgoetz1995@gmail.com>
2020-01-09 23:27:55 +0100

    chore: migrate to django 3.0.2, Close #15

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,19 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
 from fachschaftszitat import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name="home"),
-    url(r'^quote-registration/$', views.registration_quote, name="register_quote"),
-    url(r'^author-registration/$', views.registration_author, name="register_author"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('quote-registration/', views.registration_quote, name="register_quote"),
+    path('author-registration/', views.registration_author, name="register_author"),
     path('api/', include('fachschaftszitat.api.urls')),
-    path('^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
