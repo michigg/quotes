fa6364aec8f9f592db1f83e57451ba75634d46eb
MG <mgoetz1995@gmail.com>
2020-01-16 22:26:46 +0100

    chore: add id to verify user id with gui

--- src/fachschaftszitat/admin.py
+++ src/fachschaftszitat/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Quote, Statement, Author
+
 # Register your models here.
 admin.site.register(Quote)
 admin.site.register(Statement)
 admin.site.register(Author)
+
+UserAdmin.list_display = ('id',) + UserAdmin.list_display
